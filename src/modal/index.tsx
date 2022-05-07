3b08b6995528555cc6b453f9e1e8f354b167cb1a
hardmanhong <goandup@qq.com>
2022-05-07 11:41:52 +0800

    refactor: 重构 useToggle

--- src/modal/index.tsx
+++ src/modal/index.tsx
@@ -1,7 +1,7 @@
-import React, { useCallback, useEffect, useRef } from 'react'
+import React, { useCallback } from 'react'
 import { Modal } from 'antd'
 import { ModalProps } from 'antd'
-import { useToggleCom, useToggleComWithPayload } from 'u2hooks'
+import { useToggle, useToggleWithPayload } from 'u2hooks'
 
 type UModalProps = ModalProps & {
   onOk?: () => void
@@ -9,41 +9,26 @@ type UModalProps = ModalProps & {
   children?: React.ReactNode
 }
 
-type useVisibleCallback = (visible: boolean) => void
-
-type UModalFC = React.FC & {
-  useVisible: (callback: useVisibleCallback) => void
-}
-const useVisibleChange = (UModal: UModalFC, visible: boolean) => {
-  const callbackRef = useRef<useVisibleCallback>()
-  const useVisible = useCallback((callback: useVisibleCallback) => {
-    callbackRef.current = callback
-  }, [])
-  UModal.useVisible = useVisible
-  useEffect(() => {
-    callbackRef.current?.(visible)
-  }, [visible])
-}
-
 const UModal = ({ children, onOk, onCancel, ...props }: UModalProps) => {
-  const [visible, { onShow, onHide }] = useToggleCom({
+  const [visible, { onShow, onHide }] = useToggle({
     Component: UModal,
     onOk,
     onCancel
   })
-  useVisibleChange(UModal, visible)
+  UModal.useVisible = useCallback(() => visible, [visible])
   return (
     <Modal {...props} visible={visible} onOk={onShow} onCancel={onHide}>
       {children}
     </Modal>
   )
 }
+
 const usePayload = (Component: React.ReactNode, defaultPayload: any) =>
-  useToggleComWithPayload(UModal, Component, defaultPayload)
+  useToggleWithPayload(UModal, Component, defaultPayload)
 UModal.show = () => {}
 UModal.hide = () => {}
 UModal.toggle = () => {}
-UModal.useVisible = (callbck: useVisibleCallback) => {}
+UModal.useVisible = () => false
 UModal.usePayload = usePayload
 
 export default UModal
